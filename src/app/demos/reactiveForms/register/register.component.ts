45ebaaa8ad640b6440133b8d3b129c0bf268c879
Marcos Nascimento <marcosnascimento.netdev@gmail.com>
2024-06-18 08:58:31 -0300

    feat: Basic validation

--- src/app/demos/reactiveForms/register/register.component.ts
+++ src/app/demos/reactiveForms/register/register.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { FormsModule , ReactiveFormsModule , FormBuilder, FormGroup } from '@angular/forms';
+import { FormsModule , ReactiveFormsModule , FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { User } from './models/User';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [FormsModule , ReactiveFormsModule],
+  imports: [FormsModule , ReactiveFormsModule,NgClass],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
@@ -13,21 +14,27 @@ export class RegisterComponent {
 
   registerForm? : FormGroup;
   user! : User;
+  formResult : string = "";
  
   constructor(private fb: FormBuilder){ }
 
   ngOnInit(){
     this.registerForm = this.fb.group({
-      name: [""],
-      document: [""],
-      email: [""],
-      password:[""],
-      confirmPassword:[""]
+      name: ["",Validators.required],
+      document: ["",Validators.required],
+      email: ["",[Validators.required,Validators.email]],
+      password:["",Validators.required],
+      confirmPassword:["",Validators.required]
     });
   }
 
   AddUser() {
+    if(this.registerForm?.dirty && this.registerForm.valid){
       this.user = Object.assign({},this.user , this.registerForm?.value)
+      this.formResult = JSON.stringify(this.registerForm?.value);
       console.log(this.user);
+    }else{
+      this.formResult = "Não submeteu!";
+    }
   }
 } 
