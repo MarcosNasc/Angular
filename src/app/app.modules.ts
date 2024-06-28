0b9576a1a21daa3a31e3c6db792ddaa31763ed17
Marcos Nascimento <marcosnascimento.netdev@gmail.com>
2024-06-28 13:43:49 -0300

    feat: Implementation NavigationModule

--- src/app/app.modules.ts
+++ src/app/app.modules.ts
@@ -2,11 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app.routing.modules';
 import { AppComponent } from './app.component';
-import { RouterModule, RouterOutlet, provideRouter } from '@angular/router';
-import { MenuComponent } from './navegation/menu/menu.component';
-import { HomeComponent } from './institucional/home/home.component';
-import { FooterComponent } from './navegation/footer/footer.component';
-import { CommonModule, CurrencyPipe, NgClass, TitleCasePipe } from '@angular/common';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AboutComponent } from './institucional/about/about.component';
 import { ContactComponent } from './institucional/contact/contact.component';
@@ -14,13 +10,11 @@ import { ProductsComponent } from './institucional/products/products.component';
 import { provideHttpClient } from '@angular/common/http';
 import { DataBindingComponent } from './demos/data-binding/data-binding.component';
 import { RegisterComponent } from './demos/reactiveForms/register/register.component';
+import { NavigationModule } from './navegation/navigation.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent,
-    HomeComponent,
-    FooterComponent,
     AboutComponent,
     ContactComponent,
     ProductsComponent,
@@ -34,6 +28,7 @@ import { RegisterComponent } from './demos/reactiveForms/register/register.compo
     AppRoutingModule,
     RouterOutlet,
     RouterModule,
+    NavigationModule
   ],
   providers: [
     provideHttpClient()
