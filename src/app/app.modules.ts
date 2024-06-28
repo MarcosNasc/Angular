45a896c906e90abda346eb21431133c0de8f6092
Marcos Nascimento <marcosnascimento.netdev@gmail.com>
2024-06-28 13:58:19 -0300

    refactor: remove unnecessary RouterOutlet

--- src/app/app.modules.ts
+++ src/app/app.modules.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app.routing.modules';
 import { AppComponent } from './app.component';
-import { RouterOutlet } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AboutComponent } from './institucional/about/about.component';
 import { ContactComponent } from './institucional/contact/contact.component';
@@ -26,7 +25,6 @@ import { NavigationModule } from './navegation/navigation.module';
     FormsModule,
     ReactiveFormsModule,
     AppRoutingModule,
-    RouterOutlet,
     NavigationModule
   ],
   providers: [
