c1600b9f406f728b0138b39b765cb7c8677628aa
Marcos Nascimento <marcosnascimento.netdev@gmail.com>
2024-07-23 11:03:13 -0300

    feat: Implementation child routes to sub module

--- src/app/ecommerce/ecommerce-routing.module.ts
+++ src/app/ecommerce/ecommerce-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductsComponent } from './products/products.component';
+import { EditProductComponent } from './edit-product/edit-product.component';
+import { EcommerceAppComponent } from './ecommerce.app.component';
 
 const routes: Routes = [
-  {path:"" , component: ProductsComponent}
+  {path:"" , component: EcommerceAppComponent,
+    children:[
+      {path:"", component:ProductsComponent},
+      {path:"edit/:id", component:EditProductComponent}
+    ]},
 ];
 
 @NgModule({
--- src/app/ecommerce/ecommerce.module.ts
+++ src/app/ecommerce/ecommerce.module.ts
@@ -3,12 +3,16 @@ import { CommonModule } from '@angular/common';
 import { EcommerceRoutingModule } from './ecommerce-routing.module';
 import { ProductsComponent } from './products/products.component';
 import { ProductCardComponent } from './product-card/product-card.component';
+import { EditProductComponent } from './edit-product/edit-product.component';
+import { EcommerceAppComponent } from './ecommerce.app.component';
 
 
 @NgModule({
   declarations: [
+    EcommerceAppComponent,
     ProductsComponent,
     ProductCardComponent,
+    EditProductComponent,
   ],
   imports: [
     CommonModule,
