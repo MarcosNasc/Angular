418117ae02fd48273dbd8aa82feafc0ab17c8d4c
Marcos Nascimento <marcosnascimento.netdev@gmail.com>
2024-07-24 16:16:32 -0300

    feat: Implementation get route params

--- src/app/ecommerce/edit-product/edit-product.component.ts
+++ src/app/ecommerce/edit-product/edit-product.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, DEFAULT_CURRENCY_CODE, LOCALE_ID } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../products/product.service';
+import { ProductCard } from '../product-card/ProductCard';
 
 @Component({
   selector: 'app-edit-product',
   templateUrl: './edit-product.component.html',
-  styleUrl: './edit-product.component.css'
+  styleUrl: './edit-product.component.css',
+  providers: [
+    { provide: LOCALE_ID, useValue: 'pt' },
+    { provide: LOCALE_ID, useValue: 'pt-BR' },
+    { provide: DEFAULT_CURRENCY_CODE, useValue: 'BRL' },
+  ]
 })
 export class EditProductComponent {
+  
+  product ?: ProductCard;
 
+  constructor(
+    private route: ActivatedRoute, 
+    private productService : ProductService
+  )
+  {
+  }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      const id = params['id']; 
+      this.productService.getProductById(id)
+        .then(product => {
+          this.product = product;
+          // console.log(product)
+        });
+    });
+  }
+
+  salvar() : void {
+
+  }
 }
--- src/app/ecommerce/products/product.service.ts
+++ src/app/ecommerce/products/product.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Product } from './Product';
+import { ProductCard } from '../product-card/ProductCard';
 
 @Injectable({
   providedIn: 'root',
@@ -15,5 +16,10 @@ export class ProductService {
   getProducts(): Observable<Product> {
     let products = this.http.get<Product>(this.UrlServiceV1 + "/products");
     return products;
+  }
+
+  getProductById(id: number): Promise<ProductCard> {
+    return fetch(this.UrlServiceV1 + '/products/' + id)
+      .then(response => response.json())
   }
 }
